6d1b2b1dec3d1080b89dc5807b23c2e2209cccdb
Jose Duran <jooose1204@gmail.com>
2019-06-30 18:35:15 -0500

    qweqwe

--- routes/boletaConsumoRoutes.js
+++ routes/boletaConsumoRoutes.js
@@ -4,6 +4,5 @@ const boletaConsumoController = require('../controller/boletaConsumoController')
 const Router = express.Router();
 
 Router.post('/', boletaConsumoController.generarBoletaConsumo);
-Router.get('/:id', huespedController.buscarHuesped);
 
 module.exports = Router;
