4a5e6886923c53653a9e348096f538a9ecc4de4d
Jose Duran <jooose1204@gmail.com>
2019-06-29 19:47:56 -0500

    listar productos y categorias

---
+++ controller/productoController.js
@@ -0,0 +1,33 @@
+const { handleError } = require('../utils/helpers/expressHelper');
+const jwt = require('../utils/jwt');
+const _ = require('lodash');
+
+async function listarProductos(req, res) {
+  const { db } = req.app;
+  try {
+    const productos = await db.select('*').from('producto');
+    
+    return res.json({ productos, estado: 200 });
+  } catch (error) {
+    const errorMessage = handleError(error);
+    return res.json({errorMessage, estado: 500});
+  }
+}
+
+async function listarCategorias(req, res) {
+    const { db } = req.app;
+    try {
+      const productos = await db.select('*').from('categoria');
+      
+      return res.json({ categoria, estado: 200 });
+    } catch (error) {
+      const errorMessage = handleError(error);
+      return res.json({errorMessage, estado: 500});
+    }
+  }
+  
+
+module.exports = {
+  listarProductos,
+  listarCategorias
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const exampleRoutes = require('./exampleRoutes');
 const usuarioRoutes = require('./usuarioRoutes');
+const productoRoutes = require('./productoRoutes');
+
+
 const Router = express.Router();
 
 // Aqui puede configurar el Router
 Router.use('/example', exampleRoutes);
 Router.use('/usuario', usuarioRoutes);
+Router.use('/producto', productoRoutes)
 
 module.exports = Router;
