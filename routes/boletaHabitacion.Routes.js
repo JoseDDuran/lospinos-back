096d07e93164acc18c26710106e36c0c113abecd
Jose Duran <jooose1204@gmail.com>
2019-06-30 21:25:17 -0500

    adasd

--- routes/boletaHabitacion.Routes.js
+++ routes/boletaHabitacion.Routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const boletaHabitacionControllers = require('../controller/boletaHabitacionController');
+const proformaController = require('../controller/proformaController');
 
 const Router = express.Router();
 
-Router.post('/agregar', boletaHabitacionControllers.guardarProforma);
-Router.post('/anular/:id', boletaHabitacionControllers.anularProforma);
-Router.post('/procesar/:id', boletaHabitacionControllers.procesarProforma);
+Router.post('/agregar', proformaController.guardarProforma);
+Router.post('/anular/:id', proformaController.anularProforma);
+Router.post('/procesar/:id', proformaController.procesarProforma);
 Router.post('/buscar', boletaHabitacionControllers.buscarBoletaHabitacion);
 Router.post('/caducar/:id', boletaHabitacionControllers.finalizarBoletaHabitacion);
 
